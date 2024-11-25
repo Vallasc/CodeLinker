0f85117b04ff8d9d4361488cc053deb9f8ddb490
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2024-11-25 09:04:19 +0100

    feat: add read/write file utils

--- src/utils/file-utils.ts
+++ src/utils/file-utils.ts
@@ -2,20 +2,29 @@ import * as fs from "node:fs";
 import * as path from "node:path";
 import minimatch from "minimatch";
 import { IGNORE_FILE } from "../config";
+import { Extension, Language } from "../types";
 
 interface IgnorePattern {
   pattern: string;
   negated: boolean;
 }
 
+const extToLanguageMap: { [key in Extension]: Language } = {
+  ".js": "JavaScript",
+  ".ts": "TypeScript",
+  ".java": "Java",
+  ".md": "Markdown",
+  ".html": "HTML",
+};
+
 /**
  * Checks if a file matches any of the specified extensions.
- * @param filePath The file path to check.
- * @param extensions The list of allowed extensions.
+ * @param filePath - The file path to check.
+ * @param extensions - The list of allowed extensions.
  * @returns True if the file matches any extension; false otherwise.
  */
 const checkExtension = (path: string, extensions: string[]): boolean =>
-  extensions.some((ext) => path.endsWith(ext));
+  extensions.some((ext) => path.toLowerCase().endsWith(ext));
 
 /**
  * Loads ignore patterns from a configuration file (e.g., .gitignore-like file).
@@ -34,12 +43,12 @@ const loadIgnorePatterns = async (
         let outPattern = pattern;
         let negated = false;
         // Remove "!" for negated patterns
-        if(pattern.startsWith("!")){
+        if (pattern.startsWith("!")) {
           outPattern = outPattern.slice(1);
-          negated = true
+          negated = true;
         }
-        if(pattern.endsWith("/")){
-          outPattern = outPattern + "**"
+        if (pattern.endsWith("/")) {
+          outPattern = outPattern + "**";
         }
         return {
           pattern: outPattern,
@@ -56,13 +65,13 @@ const loadIgnorePatterns = async (
 
 /**
  * Determines whether a path should be ignored based on the provided ignore patterns.
- * @param itemPath The path to evaluate.
- * @param ignorePatterns The list of ignore patterns.
+ * @param itemPath - The path to evaluate.
+ * @param ignorePatterns - The list of ignore patterns.
  * @returns True if the path should be ignored; false otherwise.
  */
 const shouldIgnore = (
   itemPath: string,
-  ignorePatterns: IgnorePattern[],
+  ignorePatterns: IgnorePattern[]
 ): boolean => {
   let ignored = false;
   for (const { pattern, negated } of ignorePatterns) {
@@ -79,9 +88,9 @@ let ignorePatterns: IgnorePattern[] | null = null;
 /**
  * Recursively reads a path (file or directory) and returns a list of files
  * that match the specified extensions, while respecting ignore patterns.
- * @param inputPath The path to read.
- * @param extensions The list of file extensions to include.
- * @param ignoreFile The path to the ignore file (e.g., .gitignore).
+ * @param inputPath - The path to read.
+ * @param extensions - The list of file extensions to include.
+ * @param ignoreFile - The path to the ignore file (e.g., .gitignore).
  * @returns A promise resolving to a list of matching file paths.
  */
 export const readPath = async (
@@ -111,4 +120,46 @@ export const readPath = async (
     }
   }
   return files;
+};
+
+/**
+ * Reads the content of a file.
+ *
+ * @param filePath - The path to the file to be read.
+ * @returns A promise that resolves with the content of the file as a string.
+ */
+export const readFileAsString = async (filePath: string): Promise<string> => {
+  return fs.promises.readFile(filePath, "utf-8");
+};
+
+/**
+ * Writes content to a file.
+ *
+ * @param filePath - The path to the file to be written to.
+ * @param content - The content to write to the file.
+ * @returns A promise that resolves when the file is successfully written.
+ */
+export const writeFile = (filePath: string, content: string): Promise<void> => {
+  return fs.promises.writeFile(filePath, content, "utf-8");
+};
+
+
+export const filePathToExtension = (filePath: string): Extension => {
+  return path.extname(filePath).toLowerCase() as Extension;
+};
+
+/**
+ * Maps a file path to its corresponding programming language based on its extension.
+ *
+ * @param path - The file path to evaluate.
+ * @returns The corresponding programming language.
+ * @throws An error if the file extension is not recognized.
+ */
+export const filePathToLanguage = (path: string): Language => {
+  const ext = filePathToExtension(path);
+  // Check if the extension exists in the map
+  if (!(ext in extToLanguageMap)) {
+    return "Unknown";
+  }
+  return extToLanguageMap[ext as Extension];
 };
